0262d832b00290650020feea58f78b51e1d8e4ee
mydeveloperplanet <gunter@mydeveloperplanet.com>
2024-06-08 17:04:38 +0200

    Initial commit

---
+++ myspringbasicprojectgenerated/src/main/java/com/mydeveloperplanet/myspringbasicprojectgenerated/MySpringBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringbasicprojectgenerated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBasicProjectApplication.class, args);
+	}
+
+}
---
+++ myspringbasicprojectgenerated/src/test/java/com/mydeveloperplanet/myspringbasicprojectgenerated/MySpringBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringbasicprojectgenerated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBasicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
